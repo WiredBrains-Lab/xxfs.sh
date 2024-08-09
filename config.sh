b847f1778079447fcc236488c5dc11936182d9aa
azraq27 <bill.gross@me.com>
2024-08-09 17:00:27 -0500

    expanded 3 dirs to 5 dirs to minimize number of files per dir

--- config.sh
+++ config.sh
@@ -6,5 +6,8 @@ xxh_fname() {
     a=`echo $1 | cut -c1-1`
     b=`echo $1 | cut -c2-2`
     c=`echo $1 | cut -c3-3`
-    echo "$root_dir/$a/$b/$c/$1"
+    d=`echo $1 | cut -c4-4`
+    e=`echo $1 | cut -c5-5`
+    
+    echo "$root_dir/$a/$b/$c/$d/$e/$1"
 }
--- xxfs_ls.sh
+++ xxfs_ls.sh
@@ -16,4 +16,3 @@ else
 fi
 scan_dir=$2
 
-
